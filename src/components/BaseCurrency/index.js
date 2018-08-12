f70d01e638040082c40a59d6abafc4d58efd6e59
Angela Melinda <angela.melinda@gmail.com>
2018-08-12 19:14:40 +0700

    Update view for BaseCurrency component

--- src/components/BaseCurrency/index.js
+++ src/components/BaseCurrency/index.js
@@ -6,9 +6,20 @@ class BaseCurrency extends Component {
   }
   render() {
     return(
-      <div>
-        
+      <div className="pt-4 pb-4 mb-4 border-bottom">
+        <p className="font-italic">USD - {this.props.getCurrencyName("USD")}</p>
+        <div className="d-flex justify-content-between flex-wrap">
+          <span><strong>USD</strong></span>
+          <div>
+            <input type="text" onChange={this.props.handleChange} value={this.props.amountValue} className="pl-2 pr-2 font-weight-bold max-w-200"/>
+            {
+              this.props.alert != '' && <small className="text-right color-danger d-block">{this.props.amountAlert}</small>
+            }
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+export default BaseCurrency
