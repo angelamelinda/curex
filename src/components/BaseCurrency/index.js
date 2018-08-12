951e631873b1c011072c163d5e8992906a99050d
Angela Melinda <angela.melinda@gmail.com>
2018-08-12 19:19:25 +0700

    Change class component to functional component for Base Currency

--- src/components/BaseCurrency/index.js
+++ src/components/BaseCurrency/index.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 
-class BaseCurrency extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return(
-      <div className="pt-4 pb-4 mb-4 border-bottom">
-        <p className="font-italic">USD - {this.props.getCurrencyName("USD")}</p>
-        <div className="d-flex justify-content-between flex-wrap">
-          <span><strong>USD</strong></span>
-          <div>
-            <input type="text" onChange={this.props.handleChange} value={this.props.amountValue} className="pl-2 pr-2 font-weight-bold max-w-200"/>
-            {
-              this.props.alert != '' && <small className="text-right color-danger d-block">{this.props.amountAlert}</small>
-            }
-          </div>
+const BaseCurrency = (props) => {
+  return(
+    <div className="pt-4 pb-4 mb-4 border-bottom">
+      <p className="font-italic">USD - {props.getCurrencyName("USD")}</p>
+      <div className="d-flex justify-content-between flex-wrap">
+        <span><strong>USD</strong></span>
+        <div>
+          <input type="text" onChange={props.handleChange} value={props.amountValue} className="pl-2 pr-2 font-weight-bold max-w-200"/>
+          {
+            props.alert != '' && <small className="text-right color-danger d-block">{props.amountAlert}</small>
+          }
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default BaseCurrency
