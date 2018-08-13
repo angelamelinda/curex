01580a87657de40376acfad24b97bd8de8b6bfeb
Angela <angela@softwareseni.com>
2018-08-13 21:00:53 +0700

    Add mockData

--- src/helper/index.js
+++ src/helper/index.js
@@ -38,3 +38,37 @@ export function getCurrencyName(sym) {
         break;
     }
 }
+
+export function mockData() {
+  return {
+    // example data from redux store
+    currencies:{
+      base:"USD",
+      date:"2018-08-10",
+      rates: {
+          CAD:1.3095321229,
+          CHF:0.9943261173,
+          GBP:0.7827775838,
+          IDR:14475.8729050279,
+          INR:68.8303072626,
+          JPY:110.9200418994,
+          KRW:1129.1375698324,
+          MYR:4.0859811453,
+          SGD:1.3711592179,
+          USD:1,
+      },
+    }, listConvertedCurrency: {
+
+    },
+
+    // actions
+    RequestConvertSelectedCurrency:jest.fn(),
+    RequestDeleteCurrency:jest.fn(),
+    RequestCurrencyData:jest.fn(),
+    RequestConvertChangeAmount:jest.fn(),
+
+    // function and value passed from homepage
+    filterFloat:filterFloat,
+    amountValue:1
+  }
+}
