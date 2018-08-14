2b93fa619115eda6dc64085c157008349a4ad191
Angela <angela@softwareseni.com>
2018-08-14 07:56:25 +0700

    Update unit test for Homepage

--- src/containers/Homepage/index.test.js
+++ src/containers/Homepage/index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
 import { Homepage } from './';
+
 import * as helper from '../../helper';
 
 Enzyme.configure({ adapter: new Adapter()});
@@ -18,4 +19,14 @@ describe('Homepage', () => {
 
     expect(toJson(wrapper),{mode:'shallow'}).toMatchSnapshot();
   });
+
+  it("calls action properly", () => {
+    const wrapper = shallow(
+      <Homepage {...props} ></Homepage>
+    );
+
+    wrapper.instance().handleChange({target: {value : "1.005"}});
+
+    expect(wrapper.instance().props.RequestConvertChangeAmount).toHaveBeenCalled();
+  })
 })
